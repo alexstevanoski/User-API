6a6285fbf261bf3a913190a169b10aa3427f46a1
alexstevanoski <alexanderleigh89@gmail.com>
2023-10-19 11:33:13 +1100

    API Functional

---
+++ UserAPI/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Linq;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+
+            return Ok(payload);
+        }
+    }
+}
