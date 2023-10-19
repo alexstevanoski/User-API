5311d35ad7fc078499ca575079b8acd45dc9d7d3
alexstevanoski <alexanderleigh89@gmail.com>
2023-10-19 23:17:43 +1100

    API working

--- UserAPI/Controllers/PersonController.cs
+++ UserAPI/Controllers/PersonController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using System.Runtime.InteropServices.JavaScript;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace UserAPI.Controllers
 {
@@ -10,10 +11,18 @@ namespace UserAPI.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+
+        private List<Person> persons = new List<Person>
+        {
+            new Person {name = "Alex", email = "Alex@mail.com", id = 1, password = "examplepassword1234" },
+            new Person {name = "Joel", email = "Joel@mail.com", id = 2, password = "joels-weak-password9070" }
+        };
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello World");
+            
+            return Ok(persons);
         }
 
         [HttpPost]
---
+++ UserAPI/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace UserAPI
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public int id { get; set; }
+        public string password { get; set; }
+
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string email, int id, string password)
+        {
+            this.name = name;
+            this.email = email;
+            this.id = id;
+            this.password = password;
+        }
+    }
+}
