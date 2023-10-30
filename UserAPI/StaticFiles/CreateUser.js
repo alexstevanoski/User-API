980ef0215f14c1f9df42d9d193ae44fc272a5487
alexstevanoski <alexanderleigh89@gmail.com>
2023-10-30 22:27:03 +1100

    added creatuser js file

---
+++ UserAPI/StaticFiles/CreateUser.js
@@ -0,0 +1,14 @@
+﻿const form = document.getElementById('form1')
+
+form.addEventListener('submit', function(e) {
+    const payload = new FormData(form)
+    console.log([...payload])
+    fetch('https://localhost:7006/api/Person/CreatePerson', {
+            method: 'POST',
+            headers: { 'accept': '*/*','Content-Type': 'application/json' },
+            body: payload})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));        
+
+})
