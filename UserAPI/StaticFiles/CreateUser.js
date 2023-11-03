2032ea969a40c4916c8c131ae67f64832d5bb8b1
alexstevanoski <alexanderleigh89@gmail.com>
2023-11-03 22:47:17 +1100

    Modified html form and js

--- UserAPI/StaticFiles/CreateUser.js
+++ UserAPI/StaticFiles/CreateUser.js
@@ -1,14 +1,30 @@
-﻿const form = document.getElementById('form1')
+﻿addEventListener('DOMContentLoaded', onloadCb);
 
-form.addEventListener('submit', function(e) {
-    const payload = new FormData(form)
-    console.log([...payload])
-    fetch('https://localhost:7006/api/Person/CreatePerson', {
-            method: 'POST',
-            headers: { 'accept': '*/*','Content-Type': 'application/json' },
-            body: payload})
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));        
+function onloadCb() {
+    const forms = document.querySelectorAll('form[method]');
 
-})
+    forms.forEach(function (form) {
+        form.addEventListener('submit', displayInfo);
+    });
+};
+
+function displayInfo(event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target);
+
+    logAsTitle('Data:');
+    for (let [key, val] of formData.entries()) {
+        console.log(`${key} \t ${val || '<Empty>'}`);
+    }
+
+    logAsTitle('Form attributes');
+    const neededAttributes = ['action', 'method', 'enctype'];
+    for (let attr of neededAttributes) {
+        console.log(`${attr} - ${event.target[[attr]] || '<empty>'}`);
+    }
+}
+function logAsTitle(str) {
+    console.log('\n' + str);
+    console.log('='.repeat(str.length));
+}
